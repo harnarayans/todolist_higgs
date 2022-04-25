891ed6e6479f1100ff324f4067b6bd1e1e8cf783
Har Narayan Singh <har.narayan.singh@sap.com>
2022-04-25 21:48:53 +0530

    filter

--- script.js
+++ script.js
@@ -5,21 +5,37 @@ function keyHandler(event){
         addTodo();
     }
 }
+
+function handleSearch(event){
+    var searchText = event.target.value;
+    var filteredList = todolist.filter(item=>
+        item.indexOf(searchText)>=0
+    );
+    showList(filteredList);
+}
+
 function addTodo(){
     var inputReference = document.getElementById("inp");
     var inputValue = inputReference.value;
     todolist.push(inputValue);
-    showList();
+    showList(todolist);
     inputReference.value="";
 }
 
-function showList(){
+function handleDelete(event){
+    console.log(event.target);
+}
+
+function showList(list){
     var listRef = document.getElementById("list");
     listRef.innerHTML=null;
-    todolist.map((s)=>{
+    list.map((s)=>{
         var item = document.createElement("li");
-        item.innerHTML = s;
+        item.innerHTML = `${s} <button onClick="handleDelete(event)">Delete</button>`;
         item.classList.add("list-group-item");
+        item.classList.add("d-flex");
+        item.classList.add("justify-content-between");
+        item.classList.add("align-items-center");
         listRef.appendChild(item); 
     })
 }
